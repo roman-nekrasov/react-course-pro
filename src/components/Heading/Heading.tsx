d7e5cfefa06c28b0354af960ac79980b0b35580c
Ivan D <i.derzhylo@softpositive.com>
2022-04-26 12:28:58 +0300

    Heading component was improved

--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -2,33 +2,29 @@ import React from 'react'
 
 import s from "./Heading.module.scss"
 
-interface HeadingProps {
-    children: string;
-    h1?: boolean;
-    h2?: boolean;
-    h3?: boolean;
-    h4?: boolean;
-    h5?: boolean;
-    h6?: boolean;
-    p?: boolean;
+interface IFontSize {
+    [n: string]: string;
+}
+
+const FONT_SIZE: IFontSize = {
+    h1: "72px",
+    h2: "64px",
+    h3: "36px",
+    h4: "24px",
+    h5: "18px",
+    h6: "12px",
+    p: "16px",
 
 }
 
-const Heading = ( {children, h1 = false, h2 = false, h3= false, h4 = false, h5 = false, h6= false, p = false}: HeadingProps) => {
-    return (
-        <>
-        {
-            h1 && <h1 className={s.h1}>{children}</h1> ||
-            h2 && <h2 className={s.h2}>{children}</h2> ||
-            h3 && <h3 className={s.h3}>{children}</h3> ||
-            h4 && <h4 className={s.h4}>{children}</h4> ||
-            h5 && <h5 className={s.h5}>{children}</h5> ||
-            h6 && <h6 className={s.h6}>{children}</h6> ||
-            p && <p className={s.p}>{children}</p>
-        }
-        </>
-    )
-  
+interface HeadingProps {
+    children: string;
+    tag: string;
+
+}
+
+const Heading = ( {children, tag}: HeadingProps) => {
+   return  React.createElement(tag, {style: {fontSize: FONT_SIZE[tag]}}, children);
 }
 
 export default Heading
