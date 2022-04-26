f5c610bcd7084d4cb4948817fa9687908b113ca1
Ivan D <i.derzhylo@softpositive.com>
2022-04-26 09:28:32 +0300

    All button styles were created

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,17 +1,21 @@
 import React from 'react'
 
+import cn from 'classnames';
 import s from './Button.module.scss'
 
 interface ButtonProps {
     children: string;
     onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
+    color?: string;
+    fullSize?: boolean;
+    small?: boolean;
 }
 
-const Button: React.FC<ButtonProps> = ({ children, onClick }) => {
+const Button: React.FC<ButtonProps> = ({ children, onClick, color = "green", fullSize = false, small = false }) => {
   return (
     <button 
         type='button'
-        className={s.root}
+        className={cn(s.root, {[s.yellow]: color === "yellow", [s.fullSize]: fullSize, [s.small]: small})}
         onClick={onClick}
     >
        {children}
